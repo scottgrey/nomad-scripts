f5483133e86cd063f2b87a7cf900ab6552ecb996
Scott Grey <scottgrey@gmail.com>
2023-07-05 09:12:43 -0500

    Create validate-jobs.sh

---
+++ scripts/validate-jobs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This script is used to validate all Nomad jobs stored within this repository. When available, the NOMAD_ADDR
+# environment variable should be set to allow validation of driver configuration. Driver configuration is only
+# available if the Nomad agent is also available.
+# copied from davidsbond
+
+set -e
+
+for JOB in $(find . -type f -name '*.nomad' | sort | uniq)  ; do
+  nomad job validate "$JOB"
+done
