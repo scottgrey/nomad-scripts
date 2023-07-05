e3c7fea7c98fae0ebab57539bec83b2f7bea5029
Scott Grey <scottgrey@gmail.com>
2023-07-05 09:10:41 -0500

    Create backup-nomad-config.sh

---
+++ scripts/backup-nomad-config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This script is used to write the local nomad configuration to blob storage for backups.
+# copied from davidsbond
+
+set -e
+
+mc alias set minio "$MINIO_HOST" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+
+BUCKET_PATH=minio/nomad-backups/"$(hostname)".hcl.gz
+gzip -c /etc/nomad.d/nomad.hcl | mc pipe "$BUCKET_PATH"
